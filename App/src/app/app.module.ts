900aee1f3d8eeea929d851c4fabbb02031c0444f
hulei <hulei@nonobank.com>
2017-08-17 13:45:48 +0800

    模块定义

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProduceComponent } from './produce/produce.component';
+import { StarsComponent } from './stars/stars.component';
+import { BannerComponent } from './banner/banner.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProduceComponent,
+    StarsComponent,
+    BannerComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
---
+++ App/src/app/produce/produce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produce',
+  templateUrl: './produce.component.html',
+  styleUrls: ['./produce.component.scss']
+})
+export class ProduceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
