c0fbdc4fe85133e0ff0b8bcea6305f1b8627c3aa
hulei <hulei@nonobank.com>
2017-08-18 11:41:16 +0800

    注入FormsModule, ReactiveFormsModule

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -36,6 +37,8 @@ const appRoutes: Routes = [
     Error404Component
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
