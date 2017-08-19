f77cb60220691326eca16db1e35b0786bc93a220
hl\hl-it <523387582@qq.com>
2017-08-19 11:04:22 +0800

    响应式管道搜索

--- App/src/app/pipe/search-filter.pipe.ts
+++ App/src/app/pipe/search-filter.pipe.ts
@@ -7,9 +7,13 @@ export class SearchFilterPipe implements PipeTransform {
   // 要过滤数据,输入的参数,过滤条件
   transform(list:any[], filterField: string, keyword:string): any {
     // 为输入直接全部返回
-    if(!filterField){
+    if(!keyword){
       return list
     }
+    return list.filter( item => {
+      let v = item[filterField];
+      return v.indexOf(keyword) >= 0;
+    })
   }
 
 }
--- App/src/app/produce/produce.component.ts
+++ App/src/app/produce/produce.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService, Produce} from "../service/api.service";
-
+import {FormControl} from "@angular/forms";
+import 'rxjs/Rx';
 @Component({
   selector: 'app-produce',
   templateUrl: './produce.component.html',
@@ -10,10 +11,18 @@ export class ProduceComponent implements OnInit {
 
   public produces: Produce[];
 
-  public search: string = '';
+  public filterField: string;
+
+  // 通过管道搜索商品
+  public search: FormControl = new FormControl();
 
   //构造注册ApiService
-  constructor(public produce: ApiService) { }
+  constructor(public produce: ApiService) {
+    //在使用dubounceTime的时候需要引入 rxjs/Rx 这里的意思是订阅页面上的titleFilter变化的流,延迟500毫秒响应搜索,页面上需要属性绑定
+    //在使用响应式的表单的时候需要在app.module中引入ReactiveFormsModule
+    this.search.valueChanges.debounceTime(500)
+      .subscribe(v => this.filterField=v);
+  }
 
   ngOnInit() {
     this.produces = this.produce.getProduces();
